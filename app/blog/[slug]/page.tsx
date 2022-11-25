829bc2b1b19dd9c85e035d7192b829faffec8bba
otoyo <hiroki.t.1988@gmail.com>
2022-11-25 15:52:38 +0900

    Add page component

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,13 @@
+export async function generateStaticParams() {
+  return [{ slug: 'hello-world' }]
+}
+
+export default async function BlogSlugPage({ params }: {
+  params: { slug: string },
+}) {
+  return (
+    <>
+      <p>{params.slug}</p>
+    </>
+  )
+}

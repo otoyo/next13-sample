87ac0fea1e3009ffbd514ec1795092b2e1490f42
otoyo <hiroki.t.1988@gmail.com>
2023-01-03 13:26:30 +0900

    Edge debug

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server'
 
-type Data = {
-  name: string
-}
+export const config = { runtime: 'edge' }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler() {
+  const typicode = await (await fetch('https://jsonplaceholder.typicode.com/posts/1')).json()
+  console.log(`typicode: ${JSON.stringify(typicode)}`)
+
+  const notion = await (await fetch(`https://api.notion.com/v1/databases/${process.env.DATABASE_ID}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.NOTION_API_SECRET}`,
+      'Notion-Version': '2022-06-28',
+    },
+  })).json()
+  console.log(`Notion: ${JSON.stringify(notion)}`)
+
+  const token = btoa(`${process.env.ZENDESK_USER_EMAIL}/token:${process.env.ZENDESK_API_TOKEN}`)
+  const zendesk = await (await fetch(`https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/tickets/1.json`, {
+    headers: {
+      'Authorization': `Basic ${token}`,
+    },
+  })).json()
+  console.log(`Zendesk: ${JSON.stringify(zendesk)}`)
+
+  return NextResponse.json(
+    { status: 200 },
+  )
 }
